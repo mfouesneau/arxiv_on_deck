6e717a1e36802e63b486ac0ac91006098a0bd79f
Iskren Georgiev <iskren.y.g@gmail.com>
2019-10-03 15:34:25 +0200

    Added __ROOT_ to the destination for os.shutil.move

--- mpia.py
+++ mpia.py
@@ -173,7 +173,7 @@ def main(template=None):
                 s.compile(template=template)
                 # Generate a QR Code
                 name = s.outputname.replace('.tex', '.pdf').split('/')[-1]
-                shutil.move(__ROOT__ + '/tmp/' + name, _identifier + '.pdf', __ROOT__)
+                shutil.move(__ROOT__ + '/tmp/' + name, __ROOT__ + _identifier + '.pdf')
                 print("PDF postage:", _identifier + '.pdf' )
             else:
                 print("Not from HD... Skip.")
